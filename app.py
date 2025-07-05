a3225863da36e3e05d1711841935e7b0ba2bba59
boonjun123654 <boonjunlee@gmail.com>
2025-07-05 11:10:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from config import Config
 from models import db
+from scheduler import start_scheduler
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    start_scheduler(app)
 
     @app.route('/')
     def index():
