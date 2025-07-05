a7f6c449cf85c3b80f66a12687fc649ca5165a7f
boonjun123654 <boonjunlee@gmail.com>
2025-07-05 11:12:27 +0800

    Update models.py

--- models.py
+++ models.py
@@ -8,13 +8,14 @@ class Result2D(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     draw_date = db.Column(db.Date, nullable=False)
-    draw_no = db.Column(db.String(20), unique=True, nullable=False)  # e.g. 20250703-09
-    prize_1st = db.Column(db.String(2), nullable=False)
-    special_1 = db.Column(db.String(2), nullable=False)
-    special_2 = db.Column(db.String(2), nullable=False)
-    special_3 = db.Column(db.String(2), nullable=False)
-    special_4 = db.Column(db.String(2), nullable=False)
-    special_5 = db.Column(db.String(2), nullable=False)
+    draw_no = db.Column(db.String(20), unique=True, nullable=False)
+    prize_1st = db.Column(db.String(2))  # 最后阶段才填
+    special_1 = db.Column(db.String(2))
+    special_2 = db.Column(db.String(2))
+    special_3 = db.Column(db.String(2))
+    special_4 = db.Column(db.String(2))
+    special_5 = db.Column(db.String(2))
+    special_6 = db.Column(db.String(2))  # 新增字段
     is_even = db.Column(db.Boolean)
     is_big = db.Column(db.Boolean)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
