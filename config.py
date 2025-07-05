d61676c94602f2bb9a2dbecc644fa5a58fc9cc4e
boonjun123654 <boonjunlee@gmail.com>
2025-07-05 15:26:36 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    # Render 部署时建议使用环境变量 DATABASE_URL
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://db_4m_user:xiOe63X4iaczwTAcNfUYwS8oWrDExkHX@dpg-d11rb03uibrs73eh87vg-a/db_4m")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
