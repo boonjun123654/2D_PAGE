a124e677ddfbf4aed26ae88847a91eec7b22ff63
boonjun123654 <boonjunlee@gmail.com>
2025-07-05 13:56:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,13 @@ def create_app():
     @app.route('/latest')
     def latest():
         latest_result = Result2D.query.order_by(Result2D.id.desc()).first()
-        return render_template('latest.html', result=latest_result, getattr=getattr)
+    
+        specials = []
+        for i in range(1, 7):
+            specials.append(getattr(latest_result, f'special_{i}'))
+
+        return render_template('latest.html', result=latest_result, specials=specials)
+
 
     # ✅ 新增 AJAX API：点击按钮时调用此接口
     @app.route('/latest/step', methods=['POST'])
